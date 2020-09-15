068edde79c78c450792f9a24a3f46704defd1c8e
Elliot Wolk <elliot.wolk@gmail.com>
2020-09-15 11:11:07 -0400

    chapter-split: add komarr

---
+++ komarr/chapter-split-komarr.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+args=(
+  --artist="Lois McMaster Bujold, Grover Gardner"
+  --album="Komarr"
+
+  --shortest-chapter-len-millis=1431000
+
+  --force-chapter-break-end-seconds=19063.4 #ch10
+  --force-chapter-break-end-seconds=27892.1 #ch14
+
+  googleplay_komarr.m4a.orig
+)
+chapter-split.pl "${args[@]}" "$@"
