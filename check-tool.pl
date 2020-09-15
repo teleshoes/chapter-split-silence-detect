da3e56e2aba5f73accf6f89688127cc7f4fa3a75
Elliot Wolk <elliot.wolk@gmail.com>
2020-09-15 16:15:44 -0400

    check-tool: add script for initializing this git repo with commits so far

--- check-tool.pl
+++ check-tool.pl
@@ -3,6 +3,30 @@ use strict;
 use warnings;
 
 sub main(@){
+  gitCommitExistingDirs();
+}
+
+sub gitCommitExistingDirs(){
+  my @commands;
+  push @commands, "git reset cc7d97e09e";
+  for my $dir(glob "*/"){
+    $dir =~ s/\/$//;
+    next if $dir eq ".git";
+    my $mtime = `mtime $dir/googleplay*.m4a.orig`;
+    chomp $mtime;
+    my $mtimeP1 = $mtime+1;
+
+    push @commands, "git add $dir/chapter-split*.sh";
+    push @commands, "GIT_AUTHOR_DATE=\$(date --date=\@$mtime) git commit -m \"chapter-split: add $dir\"";
+    push @commands, "git add $dir/info-* $dir/silence-detect-interval* $dir/wav-*";
+    push @commands, "GIT_AUTHOR_DATE=\$(date --date=\@$mtimeP1) git commit -m \"info: add $dir\"";
+  }
+  push @commands, "git rebase --committer-date-is-author-date cc7d97e09e";
+
+  for my $cmd(@commands){
+    print "$cmd\n";
+    #system $cmd;
+  }
 }
 
 &main(@ARGV);
