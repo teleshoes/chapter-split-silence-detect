6367dc8043f94d7bade098e133b29c8c20760e17
Elliot Wolk <elliot.wolk@gmail.com>
2020-09-16 14:07:43 -0400

    chapter-split: redo memory with bigger min-chapter and shorter min-long

--- memory/chapter-split-memory.sh
+++ memory/chapter-split-memory.sh
@@ -3,14 +3,16 @@ args=(
   --artist="Lois McMaster Bujold, Grover Gardner"
   --album="Memory"
 
-  --shortest-chapter-len-millis=1284000
+  --long-breaks-min-silence-millis=2190
 
-  --force-chapter-break-end-seconds=1324.3 #ch2
-  --force-chapter-break-end-seconds=8927.51 #ch6
-  --force-chapter-break-end-seconds=15213.9 #ch10
-  --force-chapter-break-end-seconds=27189.9 #ch17
+  --shortest-chapter-len-millis=1323000 #except: ch18
+
   --force-chapter-break-end-seconds=30029.3 #ch19
-  --force-chapter-break-end-seconds=34203.3 #ch21
+
+  --fake-chapter-break-end-seconds=24653.7
+  --fake-chapter-break-end-seconds=27066.9
+  --fake-chapter-break-end-seconds=37582.4
+  --fake-chapter-break-end-seconds=52049.3
 
   googleplay_memory.m4a.orig
 )
